19f7c02d36e6a1ba5d331b47104b950e720a305f
Anders1605 <anders.otte@gmail.com>
2024-11-08 11:04:20 +0100

    Modul 6-1

--- modul-06/Sorting-Animation/scripts/circle-animation.js
+++ modul-06/Sorting-Animation/scripts/circle-animation.js
@@ -2,7 +2,7 @@
 const svg = d3
   .select("body")
   .append("svg")
-  .attr("width", 1000)
+  .attr("width", 2000)
   .attr("height", 1000);
 
 //Vi putter en cirkel ind i SVG-elementet
@@ -10,34 +10,35 @@ svg
   .append("circle")
   .attr("cx", 100)
   .attr("cy", 100)
-  .attr("r", 50)
-  .attr("fill", "#49d6d1");
+  .attr("r", 100)
+  .attr("fill", "#ab351b");
 
-d3.selectAll("circle")
+  d3.select("#transitionStart")
+  .on('click', function () {
+  d3.selectAll("circle")
   //En transition begynder
   .transition()
   //duration er hvor lang tid transitionen skal tage
-  .duration(2000)
+  .duration(4000)
   //herunder definerer vi slutværdierne for transitionen - cirklens cx rykkes således fra 100 - 600
-  .attr("cx", 600)
+  .attr("cx", 1200)
+  .attr("cy", 400)
   //Nu strater vi endnu en trasition som skal bevæge cirklen tilbage til 100
   .transition()
   //duration er hvor lang tid transitionen skal tage
-  .duration(2000)
+  .duration(4000)
   //herunder definerer vi slutværdierne for transitionen - cirklens cx rykkes således tilbage fra 600 - 100
-  .attr("cx", 100);
+  .attr("cx", 1200)
+  .attr("cx", 100)
 
-/**
- * Havde vi haft en knap et sted på siden, kunne vi have fået animationen til at starte ved klik på knappen:
- * d3.select("#startAnimationBtn")
-  .on("click", function () {
-    d3.selectAll("circle")
-      .transition() 
-      .duration(2000)
-      .attr("cx", 600)
-      .transition()
-      .duration(2000)
-      .attr("cx", 100)
+
+  .transition()
+  .attr("r",110)
+  .attr("fill", " #df902d ")
+
+  .transition()
+  .duration(4000)
+  .attr("r", 90)
+  .attr("cx", 100)
+  .attr("cy", 100)
   });
-  * Bonusopgave: implementer en knap som starter animationen og få cirklen til at bevæge sig i en trekant.
- */
